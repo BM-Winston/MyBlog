013fe5a9c96513427d3cb559219e043024541ffb
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-19 10:44:58 +0300

    Add URI in prodconfig

--- config.py
+++ config.py
@@ -8,11 +8,14 @@ class Config:
     
     # SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    pass
+    
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",)
 
-    pass
+    
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogs'
